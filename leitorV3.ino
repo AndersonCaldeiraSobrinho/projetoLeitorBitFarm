be634962855e31e8731785c5e45270300bd9740e
Anderson Caldeira Sobrinho <andersoncaldeirasobrinho@gmail.com>
2020-07-04 00:41:17 -0300

    Principio de todos componentes da linguagem

--- leitorV3.ino
+++ leitorV3.ino
@@ -1,6 +1,6 @@
 //Programa : RFID - Controle de Acesso leitor RFID
 //Autor : FILIPEFLOP
- 
+#include "RTClib.h"
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
@@ -10,8 +10,11 @@
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 LiquidCrystal lcd(6, 7, 5, 4, 3, 2); 
- 
+
+RTC_DS1307 rtc;
+
 char st[20];
+char daysOfTheWeek[7][12] = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"};
  
 void setup() 
 {
@@ -24,10 +27,68 @@ void setup()
   //Define o número de colunas e linhas do LCD:  
   lcd.begin(16, 2);  
   mensageminicial();
+  
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2020, 07, 03, 23, 43, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
  
 void loop() 
 {
+
+  //calcula valor da carga da bateria
+  Serial.println(float(analogRead(A0))/float(1023)*5);
+
+  //produz a impressão do calendario
+  DateTime now = rtc.now();
+
+    char horario = (now.hour(), DEC)+(':')+(now.minute(), DEC)+(':')+(now.second(), DEC);
+    char calendario = (now.day(), DEC)+('/')+(now.month(), DEC)+(now.year(), DEC);
+    char semana = (daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+  
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -63,7 +124,17 @@ void loop()
     lcd.print("Acesso liberado!");
     digitalWrite(8, HIGH);
     tone(8, 2000, 1000);
-    delay(3000);
+    delay(1000);
+    lcd.setCursor(1,0);
+    lcd.print(horario);
+    lcd.setCursor(1,1);
+    lcd.print(calendario);
+    delay(1000);
+    lcd.setCursor(2,0);
+    lcd.print(semana);
+    lcd.setCursor(2,1);
+    lcd.print(conteudo);
+    delay(1000);
     digitalWrite(8, LOW);
     mensageminicial();
   }
