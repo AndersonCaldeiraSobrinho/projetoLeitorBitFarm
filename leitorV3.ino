3a6e7a024cf9bcf66b9c8e6de5aa63261d809632
Anderson Caldeira Sobrinho <andersoncaldeirasobrinho@gmail.com>
2020-07-05 22:15:22 -0300

    construido conexão do banco de dados, aguardando para inserir conexão

--- leitorV3.ino
+++ leitorV3.ino
@@ -1,20 +1,39 @@
-//Programa : RFID - Controle de Acesso leitor RFID
-//Autor : FILIPEFLOP
+//Leitor RFID
 #include "RTClib.h"
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
+#include <Ethernet.h>
+// Define MacAddress, IP, Mascara de sub-rede e Gateway da placa de rede
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  // Define o Mac Address
+  byte ip[] = { 10, 1, 140, 57 };                       // Define o IP
+  byte subnet[] = {255, 255, 252, 0};                   // Define a máscara de rede
+  byte gateway[] = {10, 1,143, 254};                    // Define o gateway
+
+//#include <MySQL_Connection.h> //Necessario depois inserir conexão
+//#include <MySQL_Cursor.h> // Necesario depois inseir conexão
+#include <SPI.h>
  
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN);
  
 LiquidCrystal lcd(6, 7, 5, 4, 3, 2); 
 
 RTC_DS1307 rtc;
 
+char id_leitor[11] = "1";
 char st[20];
 char daysOfTheWeek[7][12] = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"};
+
+IPAddress server_addr(10,1,140,54);  // Define o Servidor de Banco de dados
+char user[] = "sistema";             // Define o usuario do Banco de Dados
+char password[] = "123456";          // Define a senha do usuario
+
+char BANCO_DE_DADOS[] = "";
+ 
+EthernetClient client;
+//MySQL_Connection conn((Client *)&client); //Inserir dados da conexão
  
 void setup() 
 {
@@ -45,7 +64,7 @@ void setup()
     //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2020, 07, 03, 23, 43, 0));
+     rtc.adjust(DateTime(2020, 07, 05, 22, 11, 10));
   }
 
   // When time needs to be re-set on a previously configured device, the
@@ -53,42 +72,57 @@ void setup()
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
-  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+   //rtc.adjust(DateTime(2020, 07, 04, 17, 59, 10));
+
+  Ethernet.begin(mac, ip, subnet, gateway);
+  
+   /*if (conn.connect(server_addr, 3306, user, password))
+      {
+      Serial.println("Conectando ao Servidor de Banco de Dados");
+
+      MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+      cur_mem->execute(BANCO_DE_DADOS);
+      delete cur_mem;
+      }
+   else
+      {
+      Serial.println("A conexão com o Banco de Dados falhou");
+      conn.close();
+      }*/ 
+      
+      //comentado para evitar erro por não estar com conexão
+   
 }
  
 void loop() 
 {
 
+  //MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+
   //calcula valor da carga da bateria
   Serial.println(float(analogRead(A0))/float(1023)*5);
 
-  //produz a impressão do calendario
+  //produz a impressão do calendario no terminal serial
   DateTime now = rtc.now();
 
-    char horario = (now.hour(), DEC)+(':')+(now.minute(), DEC)+(':')+(now.second(), DEC);
-    char calendario = (now.day(), DEC)+('/')+(now.month(), DEC)+(now.year(), DEC);
-    char semana = (daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
+    //Serial.print(now.year(), DEC);
+    //Serial.print('/');
+    //Serial.print(now.month(), DEC);
+    //Serial.print('/');
+    //Serial.print(now.day(), DEC);
+    //Serial.print(" (");
+    //Serial.print(now.hour(), DEC);
+    //Serial.print(':');
+    //Serial.print(now.minute(), DEC);
+    //Serial.print(':');
+    //Serial.print(now.second(), DEC);
+    //Serial.println();
 
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-  
+ 
+
+    String horario = (String(now.hour(), DEC))+(':')+(String(now.minute(), DEC)+(':')+(now.second(), DEC));
+    String calendario = (String (now.day(), DEC))+('/')+(String(now.month(), DEC))+('/')+(String(now.year(), DEC));
+
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -112,51 +146,43 @@ void loop()
   }
   Serial.println();
   Serial.print("Mensagem : ");
+
+
+  char INSERIR_DADOS[256]; // 256 = valor arbitrario, use uma quantidade que garanta que o texto (conteudo) nunca ira passar deste comprimento
+  snprintf_P(INSERIR_DADOS,sizeof(INSERIR_DADOS),PSTR("INSERT INTO registro_leitor_RFID (cod_leitor, cod_tag,horario, calendario) VALUES (%d, %d, %d, %d)"),id_leitor,conteudo,horario,calendario);
+     
+   //cur_mem->execute(INSERIR_DADOS); //para executar conexão
+   Serial.println("Dados gravados.");
+   
+   //delete cur_mem; //para eliminar conexão
+  
   conteudo.toUpperCase();
-  if (conteudo.substring(1) == "F2 B4 18 20") //UID 1 - Chaveiro
-  {
-    Serial.println("Ola Usuario A !");
-    Serial.println();
+    //Serial.println("Ola Usuario A !");
+    //Serial.println();
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Ola Usuario A !");
+    lcd.print(conteudo);
     lcd.setCursor(0,1);
-    lcd.print("Acesso liberado!");
+    lcd.print("Registrado");
     digitalWrite(8, HIGH);
     tone(8, 2000, 1000);
-    delay(1000);
-    lcd.setCursor(1,0);
-    lcd.print(horario);
-    lcd.setCursor(1,1);
-    lcd.print(calendario);
-    delay(1000);
-    lcd.setCursor(2,0);
-    lcd.print(semana);
-    lcd.setCursor(2,1);
-    lcd.print(conteudo);
-    delay(1000);
-    digitalWrite(8, LOW);
-    mensageminicial();
-  }
- 
-  if (conteudo.substring(1) == "BD 9B 06 7D") //UID 2 - Cartao
-  {
-    Serial.println("Ola Cartao !");
-    Serial.println();
+    delay(1500);
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Ola Cartao !");
+    lcd.print(horario);
     lcd.setCursor(0,1);
-    lcd.print("Acesso Negado !");
-    delay(3000);
+    lcd.print(calendario);
+    delay(1500);
+    digitalWrite(8, LOW);
     mensageminicial();
-  }
+    horario = "";
+    calendario = "";
 } 
  
 void mensageminicial()
 {
   lcd.clear();
-  lcd.print(" Aproxime o seu");  
+  lcd.print("Aguardando");  
   lcd.setCursor(0,1);
-  lcd.print("cartao do leitor");  
+  lcd.print("Proximo leitura");  
 }
